9b1a9e48b2ca9c0e956d99c8851acb0ed6aa8056
Damian Pramparo <damian_pramparo@hotmail.com>
2021-01-26 20:56:06 -0300

    Fixed and added temp

---
+++ API_CONF.py
@@ -0,0 +1,4 @@
+# Data for the API telegram
+class API:
+    API_TOKEN = '1574011521:AAHkoqbM_-f2jUj6Ho7sqNeD05bUMynVdcw'
+token = API
--- ServerDeptoBot.py
+++ ServerDeptoBot.py
@@ -2,23 +2,39 @@
 
 import telebot
 import time
+from pyspectator import cpu
+from API_CONF import token
 
-API_TOKEN = '1574011521:AAHkoqbM_-f2jUj6Ho7sqNeD05bUMynVdcw'
-
-bot = telebot.TeleBot(API_TOKEN)
-
+API_TOKEN = token.API_TOKEN
+try:
+    bot = telebot.TeleBot(API_TOKEN)
+    bienvenida = "Bienvenido al Bot con el que interacturá para obtener la información del servidor.̣"
+    stat = "El comando /online le permitirá acceder a ella."
+    tempe = "El comando /temp permitirá conocer la temperatura del servidor."
+except:
+    print('API not valid!!')
 # Handle '/start'
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.send_message(message.chat.id, "Bienvenido al Bot con el que interacturá para obtener la información del servidor.̣" + '\n' + "El comando /online le permitirá acceder a ella.");
-    chatid = message.chat.id
+try:
+    @bot.message_handler(commands=['start'])
+    def send_welcome(message):
+        bot.send_message(message.chat.id, bienvenida + '\n' + stat + '\n' + tempe);
+        chatid = message.chat.id
 
 
-# Handle '/online'
-@bot.message_handler(commands=['online'])
-def send_online(message):
-    while True:
-        bot.send_message(message.chat.id, "Este servidor esta online");
-        time.sleep(600)
+    # Handle '/online'
+    @bot.message_handler(commands=['online'])
+    def send_online(message):
+        while True:
+            bot.send_message(message.chat.id, "Este servidor esta online");
+            time.sleep(600)
 
-bot.polling()
+    #Handle '/temp'
+    @bot.message_handler(commands=['temperatura'])
+    def send_online(message):
+        while True:
+            bot.send_message(message.chat.id, "La temperatura es de: " + cpu.temperature);
+
+
+    bot.polling()
+except:
+    print('Error handle')
